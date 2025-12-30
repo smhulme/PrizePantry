ef4fda983ec741699835bf7d5eee4a3501bd7bc1
smhulme <smhulme101@gmail.com>
2025-12-29 23:31:25 -0800

    Initial Commit

---
+++ prizepantryTests/prizepantryTests.swift
@@ -0,0 +1,16 @@
+//
+//  prizepantryTests.swift
+//  prizepantryTests
+//
+//  Created by Shawn Hulme on 12/29/25.
+//
+
+import Testing
+
+struct prizepantryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
